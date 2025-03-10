1dd7bb9a9afaa42f6aab75043852a8659b0a9a51
Cédric Derue <cedric.derue@gmail.com>
2025-03-10 09:18:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,14 @@ resource "azurerm_static_site" "web" {
   resource_group_name = azurerm_resource_group.rg.name
   location = "eastus"
 }
+
+resource "azapi_update_resource" "web" {
+  type = "Microsoft.Web/staticSites@2024-04-01"
+  resource_id = azurerm_static_site.web.id
+  body = jsonencode({
+    properties = {
+      repositoryUrl = "https://github.com/cderue/hashitalks-2025"
+      branch = "main"
+    }
+  })
+}
