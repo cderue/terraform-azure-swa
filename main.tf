3902102fde962eec64546c8a6fdef926453d014d
Cédric Derue <cedric.derue@gmail.com>
2025-03-10 09:37:32 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_static_site" "web" {
   name = "hashitalks-2025"
   resource_group_name = azurerm_resource_group.rg.name
-  location = "eastus"
+  location = var.location
 }
 
 resource "azapi_update_resource" "web" {
