675c86720564d4165d9f01043475c603b2b89db8
Cédric Derue <cedric.derue@gmail.com>
2025-04-10 11:01:00 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+locals {
+  default_tags = {
+    environment = var.env
+    app         = var.app
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "human-talks-rg"
+  location = var.location
+
+  tags = local.default_tags
+}
+
 resource "azurerm_static_site" "web" {
   name = var.app
   resource_group_name = azurerm_resource_group.rg.name
