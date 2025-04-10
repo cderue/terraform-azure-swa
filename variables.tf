f0b499dcf4428b16c7c215ff7c0e5e14ae1cdfc2
Cédric Derue <cedric.derue@gmail.com>
2025-04-10 10:43:00 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,13 +13,3 @@ variable "app" {
   description = "Application that we want to deploy"
   type    = string
 }
-
-variable "image" {
-  description = "Docker image used for deployment"
-  type    = string
-}
-
-variable "env" {
-  description = "Application env"
-  type    = string
-}
